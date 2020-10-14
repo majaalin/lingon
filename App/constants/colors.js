20da073f9bf931a3e41393c043c717269c066cde
teresethulin <terese.thulin@gmail.com>
2020-10-14 13:57:35 +0200

    add colors

---
+++ App/constants/colors.js
@@ -0,0 +1,5 @@
+export default {
+  primary: '#E92206',
+  secondary: '#F0C4BF',
+  white: '#F8F8F8',
+};
--- App/index.js
+++ App/index.js
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import colors from './constants/colors';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.secondary,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: { color: colors.primary },
 });
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>
+        Open up App.js to start working on your app!
+      </Text>
       <StatusBar />
     </View>
   );
