94cd44c2fee3241028a5ec4cf0b6c2cf0d38b4e8
teresethulin <terese.thulin@gmail.com>
2020-10-15 16:13:38 +0200

    add basic tab and stack navigation

--- src/index.js
+++ src/index.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import { useFonts } from 'expo-font';
 import { AppLoading } from 'expo';
+import HomeScreen from './screens/HomeScreen';
+import NotesScreen from './screens/NotesScreen';
+import CalendarScreen from './screens/CalendarScreen';
 import KarlaBold from './assets/fonts/Karla-Bold.ttf';
 import KarlaRegular from './assets/fonts/Karla-Regular.ttf';
 import BrandonBold from './assets/fonts/BrandonGrotesque-Bold.ttf';
 import BrandonRegular from './assets/fonts/BrandonGrotesque-Regular.ttf';
-import colors from './styles/colors';
-import typography from './styles/typography';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: colors.secondary,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  p: {
-    fontFamily: 'KarlaRegular',
-  },
-});
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -29,17 +20,29 @@ export default function App() {
     BrandonRegular,
   });
 
+  const Tab = createBottomTabNavigator();
+  const RootStack = createStackNavigator();
+
+  const NotesStackScreen = () => (
+    <RootStack.Navigator mode="modal">
+      <RootStack.Screen
+        name="Notes"
+        component={NotesScreen}
+        options={{ headerShown: false }}
+      />
+    </RootStack.Navigator>
+  );
+
   if (!fontsLoaded) {
     return <AppLoading />;
   }
   return (
-    <View style={styles.container}>
-      <Text style={typography.h1}>Välkommen</Text>
-      <Text style={typography.h5}>
-        Svara på några frågor och vi kommer anpassa appen för dig
-      </Text>
-      <Text style={typography.buttonSecondary}>Logga in</Text>
-      <Text style={typography.buttonPrimary}>Kom igång</Text>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Överblick" component={HomeScreen} />
+        <Tab.Screen name="Anteckningar" component={NotesStackScreen} />
+        <Tab.Screen name="Kalender" component={CalendarScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
