e12dc1d7b78ca88e8728c43f4b15bbcaae612e8e
majaalin <maja_alin@hotmail.com>
2020-10-15 09:31:25 +0200

    Add fonts

--- App/index.js
+++ App/index.js
@@ -1,6 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import { AppLoading } from 'expo';
+import KarlaBold from './assets/fonts/Karla-Bold.ttf';
+import KarlaRegular from './assets/fonts/Karla-Regular.ttf';
+import BrandonBold from './assets/fonts/BrandonGrotesque-Bold.ttf';
+import BrandonRegular from './assets/fonts/BrandonGrotesque-Regular.ttf';
 
 const styles = StyleSheet.create({
   container: {
@@ -9,13 +14,31 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    fontFamily: 'KarlaBold',
+  },
+  p: {
+    fontFamily: 'KarlaRegular',
+  },
 });
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    KarlaBold,
+    KarlaRegular,
+    BrandonBold,
+    BrandonRegular,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar />
+      <Text style={styles.header}>Lingon</Text>
+      <Text style={styles.p}>Lingon</Text>
+      <Text style={{ fontFamily: 'BrandonBold' }}>Lingon</Text>
+      <Text style={{ fontFamily: 'BrandonRegular' }}>Lingon</Text>
     </View>
   );
 }
