e20018711dd81f5b2a4b66f7161e402383263c3e
majaalin <maja_alin@hotmail.com>
2020-10-20 16:11:59 +0200

    add user to firestore

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -31,6 +31,14 @@ export class Register extends Component {
       .auth()
       .createUserWithEmailAndPassword(email, password)
       .then((result) => {
+        firebase
+          .firestore()
+          .collection("users")
+          .doc(firebase.auth().currentUser.uid)
+          .set({
+            name,
+            email,
+          });
         console.log(result);
       })
       .catch((error) => {
