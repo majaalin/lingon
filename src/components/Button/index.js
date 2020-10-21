ae0ef7c20063acaa37e3b23cd36924b6ebee34b3
majaalin <maja_alin@hotmail.com>
2020-10-21 14:31:17 +0200

    add button to components

--- src/components/Button/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
-
-export default class Button extends React.Component {
-  render() {
-    return <TouchableOpacity style={styles.button} onPress={onPress} />;
-  }
-}
