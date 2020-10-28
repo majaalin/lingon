7c74029ad0f7d08d1a7331a4f4ea53b9d240ed22
teresethulin <terese.thulin@gmail.com>
2020-10-28 16:06:38 +0100

    update calendar from class component to function, add expected period dates as marked in calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,19 +1,14 @@
-import React, { Component, useEffect, useState } from 'react';
-import { firebaseAuth } from '../config/keys';
-import firebase from 'firebase';
+import React, { useReducer, useState } from 'react';
 import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { LocaleConfig } from 'react-native-calendars';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import colors from '../styles/colors';
 import typography from '../styles/typography';
-import { color } from 'react-native-reanimated';
-import { formatDistance, subDays } from 'date-fns';
-
+import { firebaseAuth } from '../config/keys';
+import firebase from 'firebase';
 const db = firebase.firestore();
 
 let ovulationDate = ['2020-10-01', '2020-10-02', '2020-10-03', '2020-10-04'];
-
-let expectedPeriodDate = ['2020-10-20'];
 
 LocaleConfig.locales['sv'] = {
   monthNames: [
@@ -48,141 +43,150 @@ LocaleConfig.locales['sv'] = {
   dayNamesShort: ['M', 'T', 'O', 'T', 'F', 'L', 'S'],
   today: 'Idag',
 };
+
 LocaleConfig.defaultLocale = 'sv';
 
-export class PeriodCalendar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      markedPeriod: null,
-      currentDate: null,
-      cycleLength: null,
-    };
-  }
+export default function PeriodCalendar() {
+  const [cycleLength, setCycleLength] = useState(0);
+  const [lastStartDate, setLastStartDate] = useState(0);
+  const [periodLength, setPeriodLength] = useState(0);
+  const [markedPeriod, setMarkedPeriod] = useState(0);
+  const [currentDate, setCurrentDate] = useState(0);
+  const [date, setDate] = useState(0);
+  const expectedPeriodDate = useState('2020-10-20', '2020-10-21', '2020-10-22');
+  const expectedOvulationDate = useState(
+    '2020-10-20',
+    '2020-10-21',
+    '2020-10-22'
+  );
 
-  componentDidMount() {
-    this.colorDate();
-  }
+  db.collection('users')
+    .doc(firebase.auth().currentUser.uid)
+    .get()
+    .then(function (doc) {
+      if (doc.exists) {
+        setCycleLength(doc.data().cycleLength);
+        setLastStartDate(doc.data().lastStartDate);
+        setPeriodLength(doc.data().periodLength);
+      } else {
+        // doc.data() will be undefined in this case
+        console.log('No such document!');
+      }
+    });
 
-  colorDate = () => {
-    let expectedPeriodDateColored = expectedPeriodDate.reduce(
-      (c, v) =>
-        Object.assign(c, {
-          [v]: {
+  // colorDate = () => {
+  //   let expectedPeriodDateColored = expectedPeriodDate.reduce(
+  //     (c, v) =>
+  //       Object.assign(c, {
+  //         [v]: {
+  //           color: colors.primary,
+  //           textColor: colors.white,
+  //           endingDay: true,
+  //           startingDay: true,
+  //         },
+  //       }),
+  //     {}
+  //   );
+
+  //   let ovulationDateColored = ovulationDate.reduce(
+  //     (c, v) =>
+  //       Object.assign(c, {
+  //         [v]: {
+  //           color: colors.secondary,
+  //           textColor: colors.white,
+  //           endingDay: true,
+  //           startingDay: true,
+  //         },
+  //       }),
+  //     {}
+  //   );
+
+  //   const markedPeriod = Object.assign(
+  //     expectedPeriodDateColored,
+  //     ovulationDateColored
+  //   );
+
+  //   setMarkedPeriod(markedPeriod);
+  // };
+
+  return (
+    <View>
+      <Text>{cycleLength}</Text>
+      <Text>{periodLength}</Text>
+      <Text>{lastStartDate}</Text>
+      <Calendar
+        horizontal={true}
+        pagingEnabled={true}
+        enableSwipeMonths={true}
+        markingType={'period'}
+        markedDates={{
+          ['2020-10-20']: {
+            selected: true,
+            startingDay: true,
             color: colors.primary,
-            textColor: colors.white,
-            endingDay: true,
-            startingDay: true,
+            disableTouchEvent: true,
+            selectedDayTextColor: colors.white,
           },
-        }),
-      {}
-    );
-
-    let ovulationDateColored = ovulationDate.reduce(
-      (c, v) =>
-        Object.assign(c, {
-          [v]: {
-            color: colors.secondary,
-            textColor: colors.white,
+          ['2020-10-21']: {
+            selected: true,
             endingDay: true,
-            startingDay: true,
+            color: colors.primary,
+            disableTouchEvent: true,
+            backgroundColor: colors.primary,
+            selectedDayTextColor: colors.white,
           },
-        }),
-      {}
-    );
-
-    const markedPeriod = Object.assign(
-      expectedPeriodDateColored,
-      ovulationDateColored
-    );
-
-    this.setState({ markedPeriod: markedPeriod });
-
-    db.collection('users')
-      .doc(firebase.auth().currentUser.uid)
-      .get()
-      .then(function (doc) {
-        if (doc.exists) {
-          let userCycleLength = doc.data().cycleLength;
-          this.setState({ cycleLength: { userCycleLength } });
-          console.log(userCycleLength);
-        } else {
-          // doc.data() will be undefined in this case
-          console.log('No such document!');
-        }
-      });
-  };
-
-  render() {
-    return (
-      <View>
-        {/* <Text>{this.state.currentDate}</Text> */}
-        {/* <Text>{lastStartDate}</Text> */}
-        <Text>{this.state.cycleLength}</Text>
-        {/* <Text>{periodLength}</Text> */}
-        <Calendar
-          horizontal={true}
-          pagingEnabled={true}
-          enableSwipeMonths={true}
-          markingType={'period'}
-          markedDates={{
-            [this.state.date]: { selected: true },
-          }}
-          markedDates={this.state.markedPeriod}
-          onDayPress={(day) => this.setState({ date: day.dateString })}
-          onDayLongPress={(day) => {
-            console.log('selected day', day.dateString);
-          }}
-          style={{
-            margin: 20,
-            borderRadius: 28,
-            height: 410,
-            width: 343,
-          }}
-          theme={{
-            backgroundColor: colors.secondary,
-            calendarBackground: colors.white,
-            textSectionTitleColor: colors.primary,
-            selectedDayBackgroundColor: colors.grey,
-            selectedDayTextColor: colors.grey,
-            todayTextColor: colors.white,
-            todayBackgroundColor: colors.grey,
-            dayTextColor: colors.primary,
-            textDisabledColor: colors.secondary,
-            dotColor: colors.primary,
-            arrowColor: colors.primary,
-            monthTextColor: colors.primary,
-            textDayFontFamily: 'KarlaRegular',
-            textMonthFontFamily: 'KarlaBold',
-            textDayHeaderFontFamily: 'KarlaRegular',
-            textDayFontSize: 16,
-            textMonthFontSize: 18,
-            textDayHeaderFontSize: 16,
-            'stylesheet.calendar.header': {
-              week: {
-                marginTop: 20,
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-              },
-              monthText: {
-                backgroundColor: colors.primary,
-                color: colors.white,
-                fontFamily: 'BrandonBold',
-                fontSize: 18,
-                margin: 10,
-                width: 106,
-                padding: 8,
-                textAlign: 'center',
-                borderRadius: 20,
-                overflow: 'hidden',
-                borderColor: colors.primary,
-              },
+        }}
+        onDayPress={(day) => setDate(day.dateString)}
+        onDayLongPress={(day) => {
+          console.log('selected day', day.dateString);
+        }}
+        style={{
+          margin: 20,
+          borderRadius: 28,
+          height: 410,
+          width: 343,
+        }}
+        theme={{
+          backgroundColor: colors.secondary,
+          calendarBackground: colors.white,
+          textSectionTitleColor: colors.primary,
+          selectedDayBackgroundColor: colors.primary,
+          selectedDayTextColor: colors.white,
+          todayTextColor: colors.white,
+          todayBackgroundColor: colors.orange,
+          dayTextColor: colors.primary,
+          textDisabledColor: colors.secondary,
+          dotColor: colors.primary,
+          arrowColor: colors.primary,
+          monthTextColor: colors.primary,
+          textDayFontFamily: 'KarlaRegular',
+          textMonthFontFamily: 'KarlaBold',
+          textDayHeaderFontFamily: 'KarlaRegular',
+          textDayFontSize: 16,
+          textMonthFontSize: 18,
+          textDayHeaderFontSize: 16,
+          'stylesheet.calendar.header': {
+            week: {
+              marginTop: 20,
+              flexDirection: 'row',
+              justifyContent: 'space-between',
             },
-          }}
-        />
-      </View>
-    );
-  }
+            monthText: {
+              backgroundColor: colors.primary,
+              color: colors.white,
+              fontFamily: 'BrandonBold',
+              fontSize: 18,
+              margin: 10,
+              width: 106,
+              padding: 8,
+              textAlign: 'center',
+              borderRadius: 20,
+              overflow: 'hidden',
+              borderColor: colors.primary,
+            },
+          },
+        }}
+      />
+    </View>
+  );
 }
-
-export default PeriodCalendar;
--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,6 +1,7 @@
 export default {
-  primary: "#E92206",
-  secondary: "#F0C4BF",
-  white: "#F8F8F8",
-  grey: "#7B6160",
+  primary: '#E92206',
+  secondary: '#F0C4BF',
+  white: '#F8F8F8',
+  grey: '#7B6160',
+  orange: '#F27649',
 };
