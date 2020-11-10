7871d9bedfb985ece473628b63da721f5fce19ec
majaalin <maja_alin@hotmail.com>
2020-11-10 15:36:53 +0100

    update bar style

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -163,7 +163,7 @@ export default function Settings({ navigation }) {
           </View>
         </View>
       </View>
-      <StatusBar barStyle="light-content" />
+      <StatusBar barStyle="dark-content" />
     </View>
   );
 }
